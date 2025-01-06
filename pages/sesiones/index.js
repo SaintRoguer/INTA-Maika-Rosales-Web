36f7effe75367d512615a753c2e29f30312e8c78
Nahuel Maika <nahuelmaika@gmail.com>
2025-01-06 15:23:12 -0300

    using serverSide to getAllSessions in "Sesiones"

--- pages/sesiones/index.js
+++ pages/sesiones/index.js
@@ -1,5 +1,6 @@
 import Admin from "layouts/Admin.js";
 import Table from "components/Table/Table.js";
+import React from "react";
 
 import useSWR from "swr";
 import moment from "moment";
@@ -11,23 +12,44 @@ import generatePdf from "../../lib/pdfGeneratorAllSessions";
 
 import MDBox from "components/MDBox";
 
-import { CircularProgress } from '@mui/material';
+import { CircularProgress } from "@mui/material";
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
 import TableContainer from "@mui/material/TableContainer";
+import { getAllSessions } from "../../lib/db-admin";
 
+export async function getServerSideProps() {
+  try {
+    const sessions = await getAllSessions();
+    return {
+      props: {
+        sessions,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching sessions:", error);
+    return {
+      props: {
+        sessions: [],
+      },
+    };
+  }
+}
 
-function Sesiones() {
-
+function Sesiones({ sessions }) {
   let csvData;
+  let tableData = getTableData(sessions);
 
-  const { data: allInfo, error: allInfoError } = useSWR(`/api/all`, {
+  React.useEffect(() => {
+    console.log(sessions);
+  }, [sessions]);
+
+  /*const { data: allInfo, error: allInfoError } = useSWR(`/api/all`, {
     refreshInterval: 1000,
   });
 
   if (allInfo) {
     csvData = { ...formatCsvDataAllSessions(allInfo) };
-  }
 
   const { data, error } = useSWR(`/api/sessions`, {
     refreshInterval: 1000,
@@ -36,46 +58,41 @@ function Sesiones() {
   if (error) return <h3>Error al cargar...</h3>;
   if (!data) {
     return <h3><CircularProgress /></h3>; //todo: Poner spinner?
-  }
-
-  let tableData = getTableData(data);
+  }*/
 
   return (
-    
-              <Table 
-                tableHead={[
-                { 
-                  accessorKey: "description",
-                  header: "Descripción",
-                },
-                { 
-                  accessorKey: "date", 
-                  header: "Fecha", 
-                  enableEditing: false,
-                },
-                { 
-                  accessorKey: "creator",
-                  header: "Creada por",
-                  enableEditing: false,
-                },
-                { 
-                accessorKey: "numberOfLotes",
-                header: "Cantidad de lotes",
-                enableEditing: false,
-                },
-                ]}
-                tableData={tableData}
-              />
-
+    <Table
+      tableHead={[
+        {
+          accessorKey: "description",
+          header: "Descripción",
+        },
+        {
+          accessorKey: "date",
+          header: "Fecha",
+          enableEditing: false,
+        },
+        {
+          accessorKey: "creator",
+          header: "Creada por",
+          enableEditing: false,
+        },
+        {
+          accessorKey: "numberOfLotes",
+          header: "Cantidad de lotes",
+          enableEditing: false,
+        },
+      ]}
+      tableData={tableData}
+    />
   );
 }
 
 function getTableData(data) {
   let tableData = [];
-  let sessionsArray = Object.values(data);
 
-  if (data && sessionsArray) {
-    sessionsArray.map((session) => {
+  if (data) {
+    data.map((session) => {
       tableData.push({
         id: session.sessionDetailId,
         sessionId: session.sessionId,
