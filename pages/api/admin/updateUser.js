cf47dc7508393335cb134ec47074af3599f5eb22
santiago rosales <anonymousvsworld@gmail.com>
2025-01-20 18:20:22 -0300

    Update user fully functioning - Only updates name, email, photoUrl and Role - Role may be removed from editable fields

---
+++ pages/api/admin/updateUser.js
@@ -0,0 +1,23 @@
+import { updateUserData } from "../../../lib/db-admin";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { uid, name, email, role, photoUrl } = req.body;
+
+  try {
+    await updateUserData({
+      uid,
+      name,
+      email,
+      role,
+      photoUrl,
+    });
+    return res.status(200).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(409).json({ error: " " +error });
+  }
+}
