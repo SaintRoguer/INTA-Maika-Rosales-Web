f670db4923a6e8e53f54d358cebb1e6df2169043
Nahuel Maika <nahuelmaika@gmail.com>
2025-05-19 19:29:26 -0300

    Fix setting no image in a profile from Admin role

--- pages/api/admin/updateUser.js
+++ pages/api/admin/updateUser.js
@@ -1,3 +1,4 @@
+import { debug } from "util";
 import { updateUserData } from "../../../lib/db-admin";
 
 export default async function handler(req, res) {
@@ -14,11 +15,11 @@ export default async function handler(req, res) {
       email,
       role,
       photoUrl,
-      password
+      password,
     });
     return res.status(200).json({ message: "User created successfully" });
   } catch (error) {
     console.error("Error creating user:", error);
-    return res.status(409).json({ error: " " +error });
+    return res.status(409).json({ error: " " + error });
   }
 }
